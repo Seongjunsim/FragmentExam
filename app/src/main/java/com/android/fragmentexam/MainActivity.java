8535ad2da6d4515eb01b91b0ffa0cd742c7c5e20
Seongjunsim <sjsim0319@gmail.com>
2020-09-01 13:46:04 +0900

    Initial commit

--- app/src/main/java/com/android/fragmentexam/MainActivity.java
+++ app/src/main/java/com/android/fragmentexam/MainActivity.java
@@ -1,7 +1,12 @@
 package com.android.fragmentexam;
 
+import android.graphics.Color;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        ColorFragment colorFragment = (ColorFragment)fragmentManager.findFragmentById(R.id.color_fragment);
+
+        colorFragment.setColor(Color.BLUE);
+    }
+
+    public void change(View view){
+        ColorFragment fragment = new ColorFragment();
+
+        int red = new Random().nextInt(256);
+        int green = new Random().nextInt(256);
+        int blue = new Random().nextInt(256);
+
+        fragment.setColor(Color.rgb(red,green,blue));
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
 }
